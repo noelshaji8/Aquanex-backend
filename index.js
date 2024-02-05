49d6f8c182ca96d3bae44432b1a9e7e5db49c9a1
noelshaji <nonvinshaji@gmail.com>
2024-02-05 22:14:42 +0530

    actuator backend

--- index.js
+++ index.js
@@ -7,7 +7,15 @@ import CircularJSON from "circular-json";
 import cron from "node-cron";
 
 const app = express();
+
 var newdata;
+var ph
+var temp;
+var acval = false;
+var automode = true;
+var actno = 1;
+var isDoneMotor = false;
+var isDoneFeeder = false;
 
 app.use(express.json());
 
@@ -31,20 +39,7 @@ initializeApp({
 });
 
 
-app.post('/send-acvalue', (req, res) => {
-  newdata = CircularJSON.stringify(req.body);
-
-  res.status(200).json(newdata);
-});
-
-
-app.get('/get-acvalue', (req, res) => {
-
-  res.json(newdata);
-
-});
-
-app.get('/get-notivalue', async (req, res) => {
+const notiTrigger = async (req, res, next) => {
 
   try {
     const apiUrl = "https://api.thingspeak.com/channels/2336234/feeds.json?api_key=ED1802UZPY1V5E5J&results=1";
@@ -55,88 +50,140 @@ app.get('/get-notivalue', async (req, res) => {
     }
 
     const data = await response.json();
-    console.log(data);
 
-    serviceFunction(data, res)
+    const temp = data["feeds"][0]["field1"];
+    const ph = data["feeds"][0]["field2"];
 
-  } catch (error) {
+    if (ph > 5 || ph < 0) {
+
+      await getMessaging().send({
+        notification: {
+          title: "pH",
+          body: "Threshold crossed",
+        },
+        token: "cfbyG9QYSvizvOzX6nphbG:APA91bGhozCWJkSgOHBBG3utPfwt9jShpQ9UriQAb3tLEkwgzoMOAZC0sjUlSGzR9z3OBG6VKl4z6dvOf-9zY6JDyXxVADEJqULImlAsR3tYDJYDphNEX6OFyEHHShAed9rBJnqagOy8",
+      })
+
+    }
+
+    if (temp > 25 || temp < 22) {
+
+      await getMessaging().send({
+        notification: {
+          title: "Temperature",
+          body: "Threshold crossed",
+        },
+        token: "cfbyG9QYSvizvOzX6nphbG:APA91bGhozCWJkSgOHBBG3utPfwt9jShpQ9UriQAb3tLEkwgzoMOAZC0sjUlSGzR9z3OBG6VKl4z6dvOf-9zY6JDyXxVADEJqULImlAsR3tYDJYDphNEX6OFyEHHShAed9rBJnqagOy8",
+      })
+    }
+
+    next();
+
+  }
+
+  catch (error) {
     console.error(error);
     res.status(500).send('Error fetching data');
   }
-});
 
-// cron.schedule("*/10 * * * * *", async (res) => {
-
-//   try {
-//     const apiUrl = "https://api.thingspeak.com/channels/2336234/feeds.json?api_key=ED1802UZPY1V5E5J&results=1";
-//     const response = await fetch(apiUrl);
-
-//     if (!response.ok) {
-//       throw new Error(`API request failed with status ${response.status}`);
-//     }
-
-//     const data = await response.json();
-//     console.log(data);
-
-//     serviceFunction(data, res)
-
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).send('Error fetching data');
-//   }
-
-// });
-
-// setInterval(async (req, res) => {
-//   try {
-//     const apiUrl = "https://api.thingspeak.com/channels/2336234/feeds.json?api_key=ED1802UZPY1V5E5J&results=1"; // Replace with the actual API endpoint
-//     const response = await fetch(apiUrl);
-
-//     if (!response.ok) {
-//       throw new Error(`API request failed with status ${response.status}`);
-//     }
-
-//     const data = await response.json();
-//     console.log(data);
-
-//     serviceFunction(data, res)
-
-
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).send('Error fetching data');
-//   }
-// }, 5000);
-
-async function serviceFunction(data, res) {
-
-  const temp = data["feeds"][0]["field1"];
-  const ph = data["feeds"][0]["field2"];
-
-  if (ph > 5 || ph < 0) {
-
-    await getMessaging().send({
-      notification: {
-        title: "pH",
-        body: "Threshold crossed",
-      },
-      token: "cfbyG9QYSvizvOzX6nphbG:APA91bGhozCWJkSgOHBBG3utPfwt9jShpQ9UriQAb3tLEkwgzoMOAZC0sjUlSGzR9z3OBG6VKl4z6dvOf-9zY6JDyXxVADEJqULImlAsR3tYDJYDphNEX6OFyEHHShAed9rBJnqagOy8",
-    })
-
-  }
-
-  if (temp > 25 || temp < 22) {
-
-    await getMessaging().send({
-      notification: {
-        title: "Temperature",
-        body: "Threshold crossed",
-      },
-      token: "cfbyG9QYSvizvOzX6nphbG:APA91bGhozCWJkSgOHBBG3utPfwt9jShpQ9UriQAb3tLEkwgzoMOAZC0sjUlSGzR9z3OBG6VKl4z6dvOf-9zY6JDyXxVADEJqULImlAsR3tYDJYDphNEX6OFyEHHShAed9rBJnqagOy8",
-    })
-  }
 
 }
+
+
+app.post('/send-acvalue', (req, res) => {
+
+  newdata = req.body;
+  acval = newdata["value"];
+  automode = newdata["automode"];
+  actno = newdata["actuator"];
+
+  console.log(newdata);
+  res.status(200).json(newdata);
+});
+
+
+app.get('/get-acvalue', notiTrigger, (req, res) => {
+
+  //NOTIFIATIONs OCCUR ONLY ONCE EVERY 10 MINS SET AKKU
+
+  if (automode) 
+  {
+    //console.log("auto");
+    if (actno == 1) 
+    {
+      //console.log("Motor");
+      if (!isDoneMotor) 
+      {
+        //console.log("reload done");
+        if (ph > 5 || ph < 0 || temp > 25 || temp < 22) 
+        {
+          //console.log("value crossed");
+          isDoneMotor = true;
+          res.json(newdata);
+          console.log(newdata);
+          setTimeout(() => { isDoneMotor = false; }, 30000); // timeout of 4 hrs
+          
+          if(acval==true)
+          {          
+            getMessaging().send({
+              notification: {
+                title: "Motor",
+                body: "Activated",
+              },
+              token: "cfbyG9QYSvizvOzX6nphbG:APA91bGhozCWJkSgOHBBG3utPfwt9jShpQ9UriQAb3tLEkwgzoMOAZC0sjUlSGzR9z3OBG6VKl4z6dvOf-9zY6JDyXxVADEJqULImlAsR3tYDJYDphNEX6OFyEHHShAed9rBJnqagOy8",
+            });
+          }
+        }
+
+      }
+      else {
+        //console.log("reload not done");
+        res.status(409).send("reload Not Done");
+      }
+    }
+    else if(actno=2)
+    {
+      //console.log("Feeder");
+      if (!isDoneFeeder) 
+      {
+        //console.log("reload done");
+        if (ph > 5 || ph < 0 || temp > 25 || temp < 22) 
+        {
+          //console.log("value crossed");
+          isDoneFeeder = true;
+          res.json(newdata);
+          console.log(newdata);
+          setTimeout(() => { isDoneFeeder = false; }, 60000); // timeout of 12 hrs
+
+          if(acval==true)
+          {          
+            getMessaging().send({
+              notification: {
+                title: "Feeder",
+                body: "Activated",
+              },
+              token: "cfbyG9QYSvizvOzX6nphbG:APA91bGhozCWJkSgOHBBG3utPfwt9jShpQ9UriQAb3tLEkwgzoMOAZC0sjUlSGzR9z3OBG6VKl4z6dvOf-9zY6JDyXxVADEJqULImlAsR3tYDJYDphNEX6OFyEHHShAed9rBJnqagOy8",
+            })
+          }
+        }
+
+      }
+      else {
+        res.status(409).send("reload Not Done");
+      }
+    }
+
+  }
+  else {
+    //console.log("not auto");
+    res.json(newdata);
+    console.log(newdata);
+  }
+
+});
+
+
+
 
 app.listen(process.env.PORT || 3000, function () {
   console.log("Server started on port 3000");
